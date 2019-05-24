8449600f7795ef19456fb97d75f2e7aa86074a74
yo <colomb.maxime@gmail.com>
2019-05-24 19:33:47 +0200

    again correction on that scala file

--- src/main/scala/mupcityplugin/EvaluateObjectiveConstruction.scala
+++ src/main/scala/mupcityplugin/EvaluateObjectiveConstruction.scala
@@ -3,8 +3,8 @@ package mupcityplugin
 import java.io.File
 //import fr.ign.task._
 
-object EvalusimulFile, folderInion {
-  def apply(simulFile : File, folderIn : File ): File = {
-    fr.ign.exp.EvaluateObjectiveConstructionWithCells(simulFile, folderIn)
+object EvaluateObjectiveConstruction {
+  def apply(simulFile : File, folderIn : File ): Double = {
+    fr.ign.exp.EvaluateObjectiveOfConstructionWithCells.EvaluateObjectiveConstructionWithCells(simulFile, folderIn)
   }
 }
